89ad4c06b14c8747e549b4c258f2d9aaba02b535
user1 <user1@user1-PC>
2017-10-26 16:54:26 +0900

    test

---
+++ semi/src/com/bbs/BoardAction.java
@@ -0,0 +1,29 @@
+package com.bbs;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.util.MyAction;
+
+public class BoardAction extends MyAction {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		String mode=req.getParameter("mode");
+		
+		if(mode==null||mode.equals("list")) {
+			//¸®½ºÆ®
+			
+			forward(req, resp, "/WEB-INF/views/bbs/list.jsp");
+			
+		}else if(mode.equals("created")) {
+			
+			forward(req,resp, "/WEB-INF/views/bbs/created.jsp");
+		}
+	}
+
+}
